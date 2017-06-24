becab153730bf74e9038c200bf9abf02afb38b5b
Konstantin Kowalski <kostelkow@gmail.com>
2017-06-24 08:50:02 -0400

    Fixed compile errors

--- test/life.asm
+++ test/life.asm
@@ -16,14 +16,22 @@ SECTION .data
    grid_size_fail_str db 'Could not determine grid size', 0xa
    grid_size_fail_str_len equ $ - grid_size_fail_str
 
+   msg_newline db 0xa
+   msg_newline_len equ $ - msg_newline
+
    exit_str db 'Exiting', 0xa
    exit_str_len equ $ - exit_str
+
+  timeval:
+      tv_sec  dd 0
+      tv_usec dd 0
 
    ;filename to load game of life from
    file_to_open db 'life.txt', 0
 
 SECTION .bss
    life_buffer resb 1
+   life_buffer_2 resb 1
 
 
 SECTION .text
@@ -46,16 +54,19 @@ _start:
 
    ;print initial board file content
 
-   pop edx ;stores length of file
+   pop edx ;stores end of file data address
    push edx ;push it for setup_gol
    sub edx, life_buffer
 
+   ;prints the current file
    mov eax, 4
    mov ebx, 1
    mov ecx, life_buffer
    int 0x80
 
-   call setup_gol
+   call print_nl
+
+   jmp setup_gol
 
    jmp exit
 
@@ -66,8 +77,7 @@ setup_gol:
       mov eax, 0 ;counter
 
       pop ecx ;file length; pushed in start
-      push ecx
-      add ecx, life_buffer ;now ecx stores end address
+      push ecx ;ecx stores end address
 
       jmp gol_grid_width_rec
       gol_grid_width_rec_ret:
@@ -80,21 +90,22 @@ setup_gol:
 
    ;START getting grid height
       mov ebx, life_buffer ;current char
-
-      mov edx, life_buffer ;edx stores when to stop counting lines
-      add edx, ecx ;add file length
+      mov edx, ecx ;edx stores end address i.e. when to stop counting
       mov eax, 0 ;ebx stores number of lines
 
       jmp gol_grid_height_rec
       gol_grid_height_rec_ret:
+
+      ;eax stores number of lines
+      push eax
    ;END getting grid height
 
-   jmp exit_with_msg
+   ;current stack: length of file, grid width grid height
+   ;jmp exit_with_msg
 
    jmp step_gol
    step_gol_ret:
 
-   ret
 
 
 gol_grid_height_rec:
@@ -109,13 +120,26 @@ gol_grid_height_rec:
    pop ebx
    inc ebx
 
-   push ebx ;;DEBUG CODE
-   add ebx, 5 ;;DEBUG CODE
-
-   ;cmp ebx, edx
+   cmp ebx, edx
    jge gol_grid_height_rec_ret
 
-   pop ebx ;;DEBUG CODE
+   ;START DEBUG
+   ;push eax
+   ;push ebx
+   ;push ecx
+   ;push edx
+   ;
+   ;mov eax, 4
+   ;mov ebx, 1
+   ;mov ecx, file_to_open
+   ;mov edx, 2
+   ;int 0x80
+   ;
+   ;pop edx
+   ;pop ecx
+   ;pop ebx
+   ;pop eax
+   ;END DEBUG
 
    jmp gol_grid_height_rec
 
@@ -134,9 +158,115 @@ gol_grid_width_rec:
    jmp gol_grid_width_rec
 
 
-step_gol:
-   jmp step_gol_ret
+;0x60 empty (`)
+;0x79 alive (x)
 
+
+step_gol_rec:
+   ;al - grid width
+   ;ah - grid height
+   ;bl - x coord
+   ;bh - y coord
+   ;ecx - file length
+
+   cmp al, bl
+   jne skip_reset_width
+   mov bl, 0
+   skip_reset_width:
+
+   cmp ah, bh
+   jne skip_reset_height
+   mov bh, 0
+   skip_reset_height:
+
+   ;old
+      ;push eax
+      ;push ebx
+
+      ;check that it's not last square
+      ;movsx edx, bl  ; edx = currRow (bl)
+      ;sub edx, 1   ; edx -= 1
+      ;mul edx, al  ; edx *= numColumns
+      ;add edx, bl  ; edx += currColumn
+
+      ;mov  dl, bl
+      ;imul dx, ax
+
+      ;imul dl, al
+      ;add  dl, bl
+   ;end old
+
+   push eax
+   push ebx
+
+   mov dl, al ;edx = gridWidth (al)
+   shr ebx, 8 ;ebx = ebx >> 8 (ebx = current y coord)
+   imul dx, bx ;edx *= currY
+
+   pop ebx ;reset eax and ebx
+   pop eax
+   push eax ;push them back on stack
+   push ebx
+
+   shl ebx, 24 ;ebx = ebx << 24
+   shr ebx, 24 ;ebx = ebx >> 24 (ebx = current x coord)
+   add edx, ebx ;edx += currX
+
+   pop eax ;reset eax and ebx
+   pop ebx
+
+   cmp edx, ecx ;compare current index with file length
+   jge step_gol_rec_ret
+
+
+
+
+
+
+
+
+step_gol:
+
+   ;current stack (from top): length of file, grid width grid height
+
+   pop eax ;eax - grid height
+   pop ebx ;ebx - grid width
+   pop ecx ;file length
+   push ecx
+   push ebx
+   push eax
+
+
+   shl eax, 8 ;grid height is in ah
+   xor eax, ebx ;grid width in al
+
+   ;mov ebx, 0
+   ;movzx ebx, 0
+   xor ebx, ebx
+
+   jmp step_gol_rec
+   step_gol_rec_ret:
+
+   pop eax ;grid height
+   pop ebx ;grid width
+   pop ecx ;file length
+   push ecx
+   push ebx
+   push eax
+
+   mov eax, 4
+   mov ebx, 1
+   mov ecx, life_buffer
+   mov edx, ecx
+   int 0x80
+
+   call print_nl
+
+   call sleep
+   jmp step_gol
+
+   call exit_with_msg
+   ;jmp step_gol_ret
 
 
 open_file:
@@ -179,6 +309,9 @@ read_file_rec:
    jmp read_file_rec
 
 
+
+;START misc functions
+
 exit_err:
 mov eax, 1
 mov ebx, 1
@@ -195,6 +328,24 @@ int_to_char:
    movzx eax, al
    ret
 
+sleep: ;this is taken from https://stackoverflow.com/questions/19580282/nasm-assembly-linux-timer-or-sleep
+   mov dword [tv_sec], 1 ;1 sec
+   mov dword [tv_usec], 0
+   mov eax, 162
+   mov ebx, timeval
+   mov ecx, 0
+   int 0x80
+   ret
+
+print_nl:
+   mov eax, 4
+   mov ebx, 1
+   mov ecx, msg_newline
+   mov edx, msg_newline_len
+   int 0x80
+   ret
+
+;END misc functions
 
 ;print_msg example:
 ;first save registers, then: print_msg 1
--- test/test.asm
+++ test/test.asm
@@ -1,1 +1,49 @@
 
+   SECTION .data ;initializing data
+
+   SECTION .bss
+
+buffer resb 1 ;1000-byte buffer in data section
+
+;uninitialized data
+
+   SECTION .text
+
+global _start
+
+_start:
+   xor eax, eax
+   ;mov al, 4
+   ;mov eax, 4
+   ;mov al, 4
+   ;mov ah, 5
+   mov eax, 4
+
+   ;shr eax, 1 ;shift right, 4 becomes 2
+   shl eax, 1 ;shift left, should become 8
+
+;   jmp int_to_char ;int_to_char receives from eax, returns in eax
+;resume_start:
+   call int_to_char
+
+   mov [buffer], eax
+
+   mov eax, 4
+   mov ebx, 1
+   mov ecx, buffer
+   mov edx, 1
+   int 0x80
+
+   mov eax, 1
+   mov ebx, 0
+   int 0x80
+
+int_to_char:
+   ;add al, 48
+   ;movzx eax, al
+   add eax, 48
+   ret
+
+
+
+
